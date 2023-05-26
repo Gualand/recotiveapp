4fd0f19ca86ac7e1836dbe926f1130c717c9ca91
Gallant Naufal Y <20106050025@student.uin-suka.ac.id>
2023-05-26 20:03:40 +0700

    tes

--- app/src/main/java/com/example/recotivapp/splashscreen.kt
+++ app/src/main/java/com/example/recotivapp/splashscreen.kt
@@ -1,11 +1,31 @@
 package com.example.recotivapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class splashscreen : AppCompatActivity() {
+
+    private lateinit var img_load:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        img_load = findViewById(R.id.imgSplash)
+
+        supportActionBar?.hide()
+
+        setAnimation()
+
+        Handler().postDelayed({
+            startActivity(Intent(this@splashscreen,MainActivity::class.java))
+        }, 5000)
+    }
+
+    private fun setAnimation(){
+        val animation = AnimationUtils.loadAnimation(this,R.anim.animation)
+        img_load.animation = animation
     }
 }
